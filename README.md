05b6e2aa18ee51d0323d64ee8e99598f314ebf99
xgroleau <xavgroleau@gmail.com>
2021-01-11 13:36:08 -0500

    Feat/init build system (#1)
    * init build system
    
    * moved to subfolder
    
    * Using library instead
    
    * Added README content
    
    * added some clarification on the README
    
    * added ci to verify compilation
    
    * renamed build CI
    
    * updated requirements
    
    * Fixed CI and fix PR comments
    
    * Fix CI

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Pheromones
-A repo that regroup SwarmUS communication messages with protobuf
+A repo that regroup SwarmUS communication messages with protobuf.
+
+
+## Requirements
+
+* `protoc` to compile .proto files
+* python libraries for protobuf to generate the files
+
+On ubuntu 20.04 you can install it using `apt install protobuf-compiler python3-protobuf`
+
+## Building 
+You can build the project using those commands
+
+```
+git clone https://github.com/SwarmUS/Pheromones
+cd Pheromones
+mkdir build && cd build
+cmake ..
+make
+```
+## Usage
+
+This project is meant to be used as a library for other project to allow serialization/deserialization. Use as a submodule or a cmake external project on the user code.
+ 
+An example is provided with the usage of the library.
---
+++ cmake/nanopb/common.cmake
@@ -0,0 +1,33 @@
+
+include(FetchContent)
+
+# Adding modules
+list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
+set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH})
+
+function(nanopb_fetch)
+    if(NANOPB_PATH)
+        message(INFO "NANOPB_PATH specified, skipping fetch")
+    endif()
+
+    FetchContent_Declare(
+        nanopb
+        GIT_REPOSITORY https://github.com/nanopb/nanopb
+        GIT_TAG        0.4.4
+        GIT_PROGRESS   TRUE
+    )
+    set(NANOPB nanopb)
+    string(TOLOWER ${NANOPB} NANOPB_L)
+
+    FetchContent_GetProperties(NANOPB POPULATED NANOPB_POPULATED)
+    if(NOT NANOPB_POPULATED)
+        message("Cloning nanopb library")
+        set(FETCHCONTENT_QUIET FALSE) # To see progress
+        FetchContent_Populate(${NANOPB})
+    endif()
+    add_subdirectory(${${NANOPB_L}_SOURCE_DIR})
+    list(APPEND CMAKE_MODULE_PATH ${${NANOPB_L}_SOURCE_DIR}/extra)
+    set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} PARENT_SCOPE)
+    set(NANOPB_PATH ${${NANOPB_L}_SOURCE_DIR} PARENT_SCOPE)
+
+endfunction()
