7d5f50301e8e060e981ce242914eb1581706b49f
xgroleau <xavgroleau@gmail.com>
2021-01-25 09:15:07 -0500

    Added options for function call

---
+++ hivemind-host/main.c
@@ -0,0 +1,13 @@
+#include <message.pb.h>
+#include <pb_decode.h>
+#include <pb_encode.h>
+#include <stdio.h>
+
+int main() {
+  /* This is the buffer where we will store our message. */
+  uint8_t buffer[128];
+  size_t message_length;
+  bool status;
+
+  return 0;
+}
